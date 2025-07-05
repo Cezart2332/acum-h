8c02731e1d51611e36bc1d919e167ab10f4e018f
Turliu Cezar <cezarturliu25@gmail.com>
2025-07-05 23:11:46 +0300

    Acoom-H update

--- backend/WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ backend/WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4ee317f670cabceb84c439f0298d7343a8e98f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57a02bea49d81727978dba3b8dd27d049b46e10")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
