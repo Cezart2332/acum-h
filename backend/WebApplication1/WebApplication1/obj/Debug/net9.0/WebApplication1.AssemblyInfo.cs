187dc34178ff9617494d4c76de7dea76b34f5106
Turliu Cezar <cezarturliu25@gmail.com>
2025-07-14 20:05:02 +0300

    new update

--- backend/WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ backend/WebApplication1/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6190a721d9e9d2b511d8d737917ea2777b9d2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21cab977d4502b7c43ac7be6069f49ee9fa674bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
